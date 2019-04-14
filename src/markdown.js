93fd1da6eafa1fbfbc9a238f181f217961585187
Sia <siakaramalegos@gmail.com>
2019-04-14 17:03:21 -0500

    Add empty check with Lodash

--- src/markdown.js
+++ src/markdown.js
@@ -1,13 +1,19 @@
 import marked from 'marked';
 import { timeNowPretty } from './time';
-// import { isEmpty } from 'lodash';
+import { isEmpty } from 'lodash';
 
 export const initializeMarkdown = () => {
   const compiled = document.getElementById('compiled-markdown');
-  const lastEdit = document.getElementById('last-edit')
+  const lastEdit = document.getElementById('last-edit');
 
   document.getElementById('editor').oninput = function (event) {
-    compiled.innerHTML = marked(event.target.value, { sanitize: true });
-    lastEdit.innerHTML = 'Last updated: ' + timeNowPretty()
+    const value = event.target.value;
+
+    if (isEmpty(value)) {
+      compiled.innerHTML = '<p>Begin writing markdown on the left to see it previewed here.</p>';
+    } else {
+      compiled.innerHTML = marked(event.target.value, { sanitize: true });
+      lastEdit.innerHTML = 'Last updated: ' + timeNowPretty();
+    }
   };
 }
