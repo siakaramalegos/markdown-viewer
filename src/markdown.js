102f944f6364ba9adfcd4d21b991e5b21ab56e1f
Sia <siakaramalegos@gmail.com>
2019-04-15 17:22:42 -0500

    Dynamically import code when needed

--- src/markdown.js
+++ src/markdown.js
@@ -1,20 +1,29 @@
-import marked from 'marked';
 import { timeNowPretty } from './time';
-// We are using this contrived Lodash example to learn about importing correctly. IRL, we should also consider whether a library is needed at all.
-import isEmpty from 'lodash/isEmpty';
 
-export const initializeMarkdown = () => {
-  const compiled = document.getElementById('compiled-markdown');
-  const lastEdit = document.getElementById('last-edit');
+async function loadMarkdownDeps() {
+  // Dynamically import lodash/isEmpty and marked
+  const { default: isEmpty } = await import('lodash/isEmpty' /* webpackChunkName: "lodash/isEmpty" */)
+  const { default: marked } = await import('marked' /* webpackChunkName: "marked" */)
 
-  document.getElementById('editor').oninput = function (event) {
-    const value = event.target.value;
+  return { isEmpty, marked }
+}
 
-    if (isEmpty(value)) {
-      compiled.innerHTML = '<p>Begin writing markdown on the left to see it previewed here.</p>';
-    } else {
-      compiled.innerHTML = marked(event.target.value, { sanitize: true });
-      lastEdit.innerHTML = 'Last updated: ' + timeNowPretty();
-    }
-  };
+export function initializeMarkdown() {
+  loadMarkdownDeps()
+    .then( ({isEmpty, marked}) => {
+      const compiled = document.getElementById('compiled-markdown');
+      const lastEdit = document.getElementById('last-edit');
+
+      document.getElementById('editor').oninput = function (event) {
+        const value = event.target.value;
+
+        if (isEmpty(value)) {
+          compiled.innerHTML = '<p>Begin writing markdown on the left to see it previewed here.</p>';
+        } else {
+          compiled.innerHTML = marked(event.target.value, { sanitize: true });
+          lastEdit.innerHTML = 'Last updated: ' + timeNowPretty();
+        }
+      }
+    })
+    .catch(error => { console.error({error})} )
 }
